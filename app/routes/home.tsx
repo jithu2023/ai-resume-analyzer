606a9d2c764544fa8202ca458d8b5406c2a60f93
jithu2023 <jithumon.j@kalvium.community>
2025-07-24 15:43:48 +0530

    home screen ui implemented

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+// home.tsx
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/Navbar";
+import ResumeCard from "../components/resumeCard";
+import { resumes } from "../../constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')]">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Application and Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+    
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+        </section>
+
+    </main>
+  );
 }
