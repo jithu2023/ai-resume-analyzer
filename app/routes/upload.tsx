06683caaae1a7f7a96807207626a12fd7fc4bc09
jithu2023 <jithumon.j@kalvium.community>
2025-07-29 14:24:51 +0530

    upload form and file uploader component

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,13 +1,26 @@
 import {useState, type FormEvent} from 'react'
 import Navbar from "../components/Navbar";
+import FileUploader from '~/components/FileUploader';
 
 
 const upload = () => {
     const[isUploading, setIsUploading] = useState(false);
     const[isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
-    const handleSubmit =(e:FormEvent<HTMLFormElement>)=>{
+    const [file, setFile] = useState<File | null>(null);
 
+    const handleFileSelect = (file: File | null) => {
+        setFile(file)
+    }
+    const handleSubmit =(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        const form =e.currentTarget.closest('form');
+        if (!form) return;
+        const formData = new FormData(form);
+        const companyName = formData.get('company-name') ;
+        const jobTitle = formData.get('Job-Ttile') ;
+        const jobDescription = formData.get('Job-Description') ;
+        console.log({ companyName, jobTitle, jobDescription, file });
     }
 
   return (
@@ -38,7 +51,7 @@ const upload = () => {
                         <textarea rows={5} name="Job-Description" placeholder='Job Description' id="Job-Description" />                  </div>
                     <div className='form-div'>
                         <label htmlFor="uploader">Upload Resume</label>
-                        <div>uploader</div>
+                        <FileUploader onFileSelect={handleFileSelect} />
                         <button className='primary-button' type='submit'>
                           Analyze Resume  
                         </button>
